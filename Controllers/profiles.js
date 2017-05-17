5b76f8731eadafcf9b4240f84a8b0e9d5a64b21c
udaypydi <udaypydi333@gmail.com>
2017-05-17 23:10:15 +0530

    Update profiles.js

--- Controllers/profiles.js
+++ Controllers/profiles.js
@@ -1,12 +1,19 @@
+import {
+  AsyncStorage,
+} from 'react-native';
+
 import Profile from '../models/profile';
 import moment from 'moment';
 
 export const index = (req, res, next) => {
-  // Find all movies and return json response
-  Profile.find({'email': 'test4@test.com'}).lean().exec((err, profiles) => res.json(
-    // Iterate through each movie
-    { profiles: profiles.map(profile => ({
-      ...profile,
-     }))}
-  ));
+
+  AsyncStorage.gettem('username').then((value) => {
+    Profile.find({'email': value}).lean().exec((err, profiles) => res.json(
+      // Iterate through each movie
+      { profiles: profiles.map(profile => ({
+        ...profile,
+       }))}
+    ));
+  };
+  }).done();
 };
